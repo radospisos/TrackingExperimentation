6a195c8e5aa6520ac041d00d92e970c47971de52
radospisos <elderlybeans@gmail.com>
2022-05-02 14:31:51 +0300

    CorrectionControllerTest was updated.

--- tests/CorrectionControllerTest.cpp
+++ tests/CorrectionControllerTest.cpp
@@ -9,21 +9,14 @@ class KCFTest : public ::testing::Test {
 };
 
 TEST_F(KCFTest, ControllerCreation) {
-    /*CorrectionController controller = CorrectionController::create(false, false, false, false);
-    EXPECT_NO_THROW(controller = CorrectionController::create(true, false, true, false));
-    EXPECT_NO_THROW(controller = CorrectionController::create(true, false, false, true));
-    EXPECT_NO_THROW(controller = CorrectionController::create(false, true, true, false));
-    EXPECT_NO_THROW(controller = CorrectionController::create(false, true, false, true));
-    EXPECT_NO_THROW(controller = CorrectionController::create(true, false, false, false));
-    EXPECT_NO_THROW(controller = CorrectionController::create(false, true, false, false));
-    EXPECT_NO_THROW(controller = CorrectionController::create(false, false, true, false));
-    EXPECT_NO_THROW(controller = CorrectionController::create(false, false, false, true));
-
-    EXPECT_THROW(controller = CorrectionController::create(true, true, true, true), std::invalid_argument);
-    EXPECT_THROW(controller = CorrectionController::create(true, true, false, true), std::invalid_argument);
-    EXPECT_THROW(controller = CorrectionController::create(true, true, true, false), std::invalid_argument);
-    EXPECT_THROW(controller = CorrectionController::create(true, true, false, false), std::invalid_argument);
-    EXPECT_THROW(controller = CorrectionController::create(true, false, true, true), std::invalid_argument);
-    EXPECT_THROW(controller = CorrectionController::create(false, true, true, true), std::invalid_argument);
-    EXPECT_THROW(controller = CorrectionController::create(false, false, true, true), std::invalid_argument);*/
+    CorrectionController controller;
+    EXPECT_NO_THROW(controller = CorrectionController(-1.0, -1.0));
+    EXPECT_NO_THROW(controller = CorrectionController(-1.0, 0.0));
+    EXPECT_NO_THROW(controller = CorrectionController(-1.0, 1.0));
+    EXPECT_NO_THROW(controller = CorrectionController(0.0, -1.0));
+    EXPECT_NO_THROW(controller = CorrectionController(0.0, 0.0));
+    EXPECT_NO_THROW(controller = CorrectionController(0.0, 1.0));
+    EXPECT_NO_THROW(controller = CorrectionController(1.0, -1.0));
+    EXPECT_NO_THROW(controller = CorrectionController(1.0, 0.0));
+    EXPECT_NO_THROW(controller = CorrectionController(1.0, 1.0));
 }
