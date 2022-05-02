067b39ea443b3e4c64d1df01fb93483dfbc4f75e
radospisos <elderlybeans@gmail.com>
2022-05-02 14:46:51 +0300

    Added tests to CorrectionControllerTest.

--- tests/CorrectionControllerTest.cpp
+++ tests/CorrectionControllerTest.cpp
@@ -20,3 +20,15 @@ TEST_F(KCFTest, ControllerCreation) {
     EXPECT_NO_THROW(controller = CorrectionController(1.0, 0.0));
     EXPECT_NO_THROW(controller = CorrectionController(1.0, 1.0));
 }
+
+TEST_F(KCFTest, FieldAccess1) {
+    CorrectionController controller;
+    ASSERT_EQ(controller.dx(), 0.0);
+    ASSERT_EQ(controller.dy(), 0.0);
+}
+
+TEST_F(KCFTest, FieldAccess2) {
+    CorrectionController controller{25.0, -27.0};
+    ASSERT_EQ(controller.dx(), 25.0);
+    ASSERT_EQ(controller.dy(), -27.0);
+}
